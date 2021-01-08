e7d61e3a85f07cb9a5d712afcc94e3b045c735eb
MHammadYou <MHammadY2.0@gmail.com>
2021-01-08 22:46:52 +0500

    Fixed empty forms changing the poll count

--- core/views.py
+++ core/views.py
@@ -6,23 +6,27 @@ from polls.models import Poll as Poll_Model
 
 def index(request):
     if request.method == 'POST':
+        print(request.POST.get('option_1'))
+        print(request.POST.get('option_2'))
 
-        if request.user.username:
-            poll_data = request.POST
-            poll_obj = Poll_Model.objects.filter(id=poll_data.get('id')).first()
+        if request.POST.get('option_1') and request.POST.get('option_2'):
 
-            if request.user in poll_obj.voters.all():
-                messages.error(request, 'You can only vote once')
-            else:
-                if poll_data.get('choice') == poll_obj.option_1:
-                    poll_obj.count_1 += 1
+            if request.user.username:
+                poll_data = request.POST
+                poll_obj = Poll_Model.objects.filter(id=poll_data.get('id')).first()
+
+                if request.user in poll_obj.voters.all():
+                    messages.error(request, 'You can only vote once')
                 else:
-                    poll_obj.count_2 += 1
-                poll_obj.voters.add(request.user)
-                poll_obj.save()
-                messages.success(request, 'Voted Successfully')
-        else:
-            messages.error(request, 'Please login to vote')
+                    if poll_data.get('choice') == poll_obj.option_1:
+                        poll_obj.count_1 += 1
+                    else:
+                        poll_obj.count_2 += 1
+                    poll_obj.voters.add(request.user)
+                    poll_obj.save()
+                    messages.success(request, 'Voted Successfully')
+            else:
+                messages.error(request, 'Please login to vote')
 
     context = {
         'title': 'Make Me Choose',
